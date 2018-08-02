boldtrn/mapbox-navigation-android

replace gpx animator with replay location dispatcher (runnable + handler)

libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayLocationMapper.java
@@ -1,0 +1,55 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import android.location.Location;
+import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReplayLocationMapper {
+
+  private static final String NON_NULL_AND_NON_EMPTY_REPLAY_LOCATION_LIST_REQUIRED = "Non-null and non-empty replay "
+    + "location list required.";
+  private static final String REPLAY = "ReplayLocation";
+  private final List<ReplayLocationDto> replayLocations;
+
+  public ReplayLocationMapper(List<ReplayLocationDto> replayLocations) {
+    checkValidInput(replayLocations);
+    this.replayLocations = replayLocations;
+  }
+
+  public List<Location> toLocations() {
+    List<Location> mappedLocations = mapReplayLocations();
+    return mappedLocations;
+  }
+
+  private void checkValidInput(List<ReplayLocationDto> locations) {
+    boolean isValidInput = locations == null || locations.isEmpty();
+    if (isValidInput) {
+      throw new IllegalArgumentException(NON_NULL_AND_NON_EMPTY_REPLAY_LOCATION_LIST_REQUIRED);
+    }
+  }
+
+  private List<Location> mapReplayLocations() {
+    List<Location> locations = new ArrayList<>(replayLocations.size());
+    for (ReplayLocationDto sample : replayLocations) {
+      Location location = new Location(REPLAY);
+      location.setLongitude(sample.getLongitude());
+      location.setAccuracy(sample.getHorizontalAccuracyMeters());
+      location.setBearing((float) sample.getBearing());
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        location.setVerticalAccuracyMeters(sample.getVerticalAccuracyMeters());
+      }
+      location.setSpeed((float) sample.getSpeed());
+      location.setLatitude(sample.getLatitude());
+      location.setAltitude(sample.getAltitude());
+      Date date = sample.getTime();
+      if (date != null) {
+        location.setTime(date.getTime());
+      }
+      locations.add(location);
+    }
+    return locations;
+  }
+}

libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayLocationMapperTest.java
@@ -1,0 +1,178 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import android.location.Location;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class ReplayLocationMapperTest {
+
+  private static final double DELTA = 1e-15;
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checksNonNullLocationListRequired() {
+    List<ReplayLocationDto> nullLocations = null;
+
+    new ReplayLocationMapper(nullLocations);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checksNonEmptyLocationListRequired() {
+    List<ReplayLocationDto> empty = Collections.emptyList();
+
+    new ReplayLocationMapper(empty);
+  }
+
+  @Test
+  public void checksProviderMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals("ReplayLocation", theLocation.getProvider());
+  }
+
+  @Test
+  public void checksLongitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setLongitude(2.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(2.0, theLocation.getLongitude(), DELTA);
+  }
+
+  @Test
+  public void checksAccuracyMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setHorizontalAccuracyMeters(3.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(3.0f, theLocation.getAccuracy(), DELTA);
+  }
+
+  @Test
+  public void checksBearingMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setBearing(180.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(180f, theLocation.getBearing(), DELTA);
+  }
+
+  @Test
+  @Config(sdk = 26)
+  public void checksVerticalAccuracyMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setVerticalAccuracyMeters(8.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(8.0, theLocation.getVerticalAccuracyMeters(), DELTA);
+  }
+
+  @Test(expected = NoSuchMethodError.class)
+  @Config(sdk = 25)
+  public void checksVerticalAccuracyNotMappedForBelowOreo() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setVerticalAccuracyMeters(8.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    theLocation.getVerticalAccuracyMeters();
+  }
+
+  @Test
+  public void checksSpeedMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setSpeed(65.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(65.0f, theLocation.getSpeed(), DELTA);
+  }
+
+  @Test
+  public void checksLatitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setLatitude(7.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(7.0, theLocation.getLatitude(), DELTA);
+  }
+
+  @Test
+  public void checksAltitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setAltitude(9.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(9.0, theLocation.getAltitude(), DELTA);
+  }
+
+  @Test
+  public void checksTimeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    Date aDate = new Date();
+    aReplayLocation.setTime(aDate);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    long timeFromDate = aDate.getTime();
+    assertEquals(timeFromDate, theLocation.getTime(), DELTA);
+  }
+}

libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayRouteParserTest.java
@@ -1,0 +1,120 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import com.google.gson.GsonBuilder;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReplayRouteParserTest {
+
+  private static final double DELTA = 1e-15;
+
+  @Test
+  public void checksLongitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(11.579233823791801, firstLocation.getLongitude(), DELTA);
+  }
+
+  @Test
+  public void checksHorizontalAccuracyParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(40, firstLocation.getHorizontalAccuracyMeters(), DELTA);
+  }
+
+  @Test
+  public void checksBearingParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(277.0355517432898, firstLocation.getBearing(), DELTA);
+  }
+
+  @Test
+  public void checksVerticalAccuracyParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(10, firstLocation.getVerticalAccuracyMeters(), DELTA);
+  }
+
+  @Test
+  public void checksSpeedParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(14.704089336389941, firstLocation.getSpeed(), DELTA);
+  }
+
+  @Test
+  public void checksLatitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(48.1776966801359, firstLocation.getLatitude(), DELTA);
+  }
+
+  @Test
+  public void checksAltitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(0, firstLocation.getAltitude(), DELTA);
+  }
+
+  @Test
+  public void checksTimestampParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    String dateFormatPattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatPattern);
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    assertEquals("2018-06-25T18:16:11.005+0000", dateFormat.format(firstLocation.getTime()));
+  }
+
+  @Test
+  public void checksRouteParsing() {
+    String json = obtainJson("reroute.json");
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+    assertEquals("https://api.mapbox.com/directions/v5/mapbox/driving-traffic/11.579233823791801,48.1776966801359;" +
+      "11.573521553454881,48.17812728496367.json?access_token=pk" +
+      ".eyJ1IjoibWFwYm94LW5hdmlnYXRpb24iLCJhIjoiY2plZzkxZnl4MW9tZDMzb2R2ZXlkeHlhbCJ9.L1c9Wo-gk6d3cR3oi1n9SQ&steps" +
+      "=true&overview=full&geometries=geojson", routeFromJson.getRouteRequest());
+  }
+
+  private String obtainJson(String fileName) {
+    ClassLoader classLoader = getClass().getClassLoader();
+    return convertStreamToString(classLoader.getResourceAsStream(fileName));
+  }
+
+  private String convertStreamToString(InputStream is) {
+    Scanner s = new Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+  }
+}

mayardb/mapbox-navigation-android-1

libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayLocationMapper.java
@@ -1,0 +1,55 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import android.location.Location;
+import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ReplayLocationMapper {
+
+  private static final String NON_NULL_AND_NON_EMPTY_REPLAY_LOCATION_LIST_REQUIRED = "Non-null and non-empty replay "
+    + "location list required.";
+  private static final String REPLAY = "ReplayLocation";
+  private final List<ReplayLocationDto> replayLocations;
+
+  public ReplayLocationMapper(List<ReplayLocationDto> replayLocations) {
+    checkValidInput(replayLocations);
+    this.replayLocations = replayLocations;
+  }
+
+  public List<Location> toLocations() {
+    List<Location> mappedLocations = mapReplayLocations();
+    return mappedLocations;
+  }
+
+  private void checkValidInput(List<ReplayLocationDto> locations) {
+    boolean isValidInput = locations == null || locations.isEmpty();
+    if (isValidInput) {
+      throw new IllegalArgumentException(NON_NULL_AND_NON_EMPTY_REPLAY_LOCATION_LIST_REQUIRED);
+    }
+  }
+
+  private List<Location> mapReplayLocations() {
+    List<Location> locations = new ArrayList<>(replayLocations.size());
+    for (ReplayLocationDto sample : replayLocations) {
+      Location location = new Location(REPLAY);
+      location.setLongitude(sample.getLongitude());
+      location.setAccuracy(sample.getHorizontalAccuracyMeters());
+      location.setBearing((float) sample.getBearing());
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        location.setVerticalAccuracyMeters(sample.getVerticalAccuracyMeters());
+      }
+      location.setSpeed((float) sample.getSpeed());
+      location.setLatitude(sample.getLatitude());
+      location.setAltitude(sample.getAltitude());
+      Date date = sample.getDate();
+      if (date != null) {
+        location.setTime(date.getTime());
+      }
+      locations.add(location);
+    }
+    return locations;
+  }
+}

libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayLocationMapperTest.java
@@ -1,0 +1,178 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import android.location.Location;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class ReplayLocationMapperTest {
+
+  private static final double DELTA = 1e-15;
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checksNonNullLocationListRequired() {
+    List<ReplayLocationDto> nullLocations = null;
+
+    new ReplayLocationMapper(nullLocations);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void checksNonEmptyLocationListRequired() {
+    List<ReplayLocationDto> empty = Collections.emptyList();
+
+    new ReplayLocationMapper(empty);
+  }
+
+  @Test
+  public void checksProviderMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals("ReplayLocation", theLocation.getProvider());
+  }
+
+  @Test
+  public void checksLongitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setLongitude(2.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(2.0, theLocation.getLongitude(), DELTA);
+  }
+
+  @Test
+  public void checksAccuracyMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setHorizontalAccuracyMeters(3.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(3.0f, theLocation.getAccuracy(), DELTA);
+  }
+
+  @Test
+  public void checksBearingMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setBearing(180.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(180f, theLocation.getBearing(), DELTA);
+  }
+
+  @Test
+  @Config(sdk = 26)
+  public void checksVerticalAccuracyMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setVerticalAccuracyMeters(8.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(8.0, theLocation.getVerticalAccuracyMeters(), DELTA);
+  }
+
+  @Test(expected = NoSuchMethodError.class)
+  @Config(sdk = 25)
+  public void checksVerticalAccuracyNotMappedForBelowOreo() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setVerticalAccuracyMeters(8.0f);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    theLocation.getVerticalAccuracyMeters();
+  }
+
+  @Test
+  public void checksSpeedMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setSpeed(65.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(65.0f, theLocation.getSpeed(), DELTA);
+  }
+
+  @Test
+  public void checksLatitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setLatitude(7.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(7.0, theLocation.getLatitude(), DELTA);
+  }
+
+  @Test
+  public void checksAltitudeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    aReplayLocation.setAltitude(9.0);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    assertEquals(9.0, theLocation.getAltitude(), DELTA);
+  }
+
+  @Test
+  public void checksTimeMapping() {
+    List<ReplayLocationDto> anyReplayLocations = new ArrayList<>(1);
+    ReplayLocationDto aReplayLocation = new ReplayLocationDto();
+    Date aDate = new Date();
+    aReplayLocation.setDate(aDate);
+    anyReplayLocations.add(aReplayLocation);
+    ReplayLocationMapper theReplayLocationMapper = new ReplayLocationMapper(anyReplayLocations);
+
+    List<Location> locations = theReplayLocationMapper.toLocations();
+
+    Location theLocation = locations.get(0);
+    long timeFromDate = aDate.getTime();
+    assertEquals(timeFromDate, theLocation.getTime(), DELTA);
+  }
+}

libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayRouteParserTest.java
@@ -1,0 +1,120 @@
+package com.mapbox.services.android.navigation.v5.location.replay;
+
+import com.google.gson.GsonBuilder;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReplayRouteParserTest {
+
+  private static final double DELTA = 1e-15;
+
+  @Test
+  public void checksLongitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(11.579233823791801, firstLocation.getLongitude(), DELTA);
+  }
+
+  @Test
+  public void checksHorizontalAccuracyParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(40, firstLocation.getHorizontalAccuracyMeters(), DELTA);
+  }
+
+  @Test
+  public void checksBearingParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(277.0355517432898, firstLocation.getBearing(), DELTA);
+  }
+
+  @Test
+  public void checksVerticalAccuracyParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(10, firstLocation.getVerticalAccuracyMeters(), DELTA);
+  }
+
+  @Test
+  public void checksSpeedParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(14.704089336389941, firstLocation.getSpeed(), DELTA);
+  }
+
+  @Test
+  public void checksLatitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(48.1776966801359, firstLocation.getLatitude(), DELTA);
+  }
+
+  @Test
+  public void checksAltitudeParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    assertEquals(0, firstLocation.getAltitude(), DELTA);
+  }
+
+  @Test
+  public void checksTimestampParsing() {
+    String json = obtainJson("reroute.json");
+
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+
+    ReplayLocationDto firstLocation = routeFromJson.getLocations().get(0);
+    String dateFormatPattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatPattern);
+    dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    assertEquals("2018-06-25T18:16:11.005+0000", dateFormat.format(firstLocation.getDate()));
+  }
+
+  @Test
+  public void checksRouteParsing() {
+    String json = obtainJson("reroute.json");
+    ReplayRouteDto routeFromJson = new GsonBuilder().create().fromJson(json, ReplayRouteDto.class);
+    assertEquals("https://api.mapbox.com/directions/v5/mapbox/driving-traffic/11.579233823791801,48.1776966801359;" +
+      "11.573521553454881,48.17812728496367.json?access_token=pk" +
+      ".eyJ1IjoibWFwYm94LW5hdmlnYXRpb24iLCJhIjoiY2plZzkxZnl4MW9tZDMzb2R2ZXlkeHlhbCJ9.L1c9Wo-gk6d3cR3oi1n9SQ&steps" +
+      "=true&overview=full&geometries=geojson", routeFromJson.getRouteRequest());
+  }
+
+  private String obtainJson(String fileName) {
+    ClassLoader classLoader = getClass().getClassLoader();
+    return convertStreamToString(classLoader.getResourceAsStream(fileName));
+  }
+
+  private String convertStreamToString(InputStream is) {
+    Scanner s = new Scanner(is).useDelimiter("\\A");
+    return s.hasNext() ? s.next() : "";
+  }
+}